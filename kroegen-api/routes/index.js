a07a85b2631259e7ca948f7bce8d574704efc729
vegatarier <sam.sam.s@hotmail.com>
2016-03-14 14:09:30 +0100

    spul voor sockets

--- kroegen-api/routes/index.js
+++ kroegen-api/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+io.on('connection',function(socket){});
+
+server.listen(3000);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
