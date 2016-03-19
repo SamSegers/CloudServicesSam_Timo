06dc47336a70e044519e5df83aa4ca7e6d1bcd3f
vegatarier <sam.sam.s@hotmail.com>
2016-03-19 21:17:44 +0100

    sockets

--- kroegen-api/routes/index.js
+++ kroegen-api/routes/index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var server = require('http').createServer();
-var io = require('socket.io')(server);
+var http = require("http");
+var io = require('socket.io');
+var url = require('url');
 
-io.on('connection',function(socket){});
+var server = http.createServer(function(request,response)
+{
+	var path = url.parse(request.url).pathname;
+	
+	switch(path)
+	{
+		default:
+		response.writeHead(404);
+		response.end();
+			break;
+	}
+});
+
+//io.on('connection',function(socket){});
 
 server.listen(3000);
 
+var listener = io.listen(server);
+listener.sockets.on('connection',function(socket){
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
