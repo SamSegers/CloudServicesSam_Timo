00311e490bd01289311db161c43c607358d36c46
vegatarier <sam.sam.s@hotmail.com>
2016-03-20 21:37:31 +0100

    googleplacestoegevoegd

---
+++ kroegen-api/models/googleplaces.js
@@ -0,0 +1,33 @@
+function getplaces()
+{
+	$.ajax({
+            url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDdCI0Csk73tnbL-nkBIPXu26U6pvb1weA&location=denbosch&radius=1000&type=cafe",
+            data: "",
+            contentType: "application/json; charset=utf-8", 
+            type: "GET",
+			async: true,
+            success: function (data) {
+				alert(data);
+            },
+            error: function () {
+
+            }
+        });
+}
+
+function getplacedetails(id)
+{
+	$.ajax({
+            url: "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDdCI0Csk73tnbL-nkBIPXu26U6pvb1weA&placeid="+id,
+            data: "",
+            contentType: "application/json; charset=utf-8", 
+            type: "GET",
+			async: true,
+            success: function (data) {
+				alert(data);
+            },
+            error: function () {
+
+            }
+        });
+}
