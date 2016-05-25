66ddca0eb5d8af6bda94502a4a1db3ec5a56da99
Timo Hoff <timohoff@live.nl>
2016-05-25 15:44:15 +0200

    front-end improved authentication style

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,55 @@
 "use strict"
 
-let $signUpForm = $('.sign-up-form');
-let $signInForm = $('.sign-in-form');
+let $section = {
+	menu: $('section.menu'),
+	signUp: $('section.form.sign-up'),
+	signIn: $('section.form.sign-in'),
+	message: $('section.message')
+}
 
-$('.sign-up').click(function(){
+$section.menu.find('.sign-up').click(function(){
+	$section.signUp.show();	
+	$section.signIn.hide();
+});
+
+$section.menu.find('.sign-in').click(function(){
+	$section.signIn.show();	
+	$section.signUp.hide();
+});
+
+$section.menu.find('.sign-out').click(function(){
+	$section.menu.find('.username').hide();
+	$(this).hide();
+	$section.message.html('signed out');
+});
+
+$section.signIn.find('.button > input').click(function(){
 	let body = {
-		username: $signUpForm.find('.username').val(),
-		password: $signUpForm.find('.password').val()
+		username: $section.signIn.find('.username > input').val(),
+		password: $section.signIn.find('.password > input').val()
 	}
 
-	$.post('http://localhost:3001/signup', body, function(data){
+	$.post('http://localhost:3001/login', body, function(data){
+		$section.message.html('signed in');
+		$section.menu.find('.username').text(data.username).show();
+		$section.menu.find('.sign-out').show();
+		$section.signIn.hide();
 		console.log(data);
 	});
 });
 
-$('.sign-in').click(function(){
+$section.signUp.find('.button > input').click(function(){
 	let body = {
-		username: $signInForm.find('.username').val(),
-		password: $signInForm.find('.password').val()
+		username: $section.signUp.find('.username > input').val(),
+		password: $section.signUp.find('.password > input').val()
 	}
+	let passwordRepeat = $section.signUp.find('.password-repeat').val();
 
-	$.post('http://localhost:3001/login', body, function(data){
-		console.log(data);
-	});
+	if(body.password==passwordRepeat){
+		$.post('http://localhost:3001/signup', body, function(data){
+			$section.message.html('signed up');
+			$section.signUp.hide();
+			console.log(data);
+		});
+	}else $section.message.html('repeated password does not match');
 });
